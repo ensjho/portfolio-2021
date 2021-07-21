b4f9c82b5797fa1cf0e28b29b9e2a7b483591de0
Eric <ejho13@berkeley.edu>
2021-07-20 23:36:11 -0700

    light/night mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import EricCardCase from './EricCardCase';
+import "./App.css";
+import EricCardCase from "./EricCardCase";
 
 function App() {
   return (
     <div className="App">
       <EricCardCase />
+      <input type="checkbox" id="toggle" class="toggle--checkbox" />
+      <label for="toggle" class="toggle--label">
+        <span class="toggle--label-background"></span>
+      </label>
+      <div class="background"></div>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
